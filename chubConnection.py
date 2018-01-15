127bb5ff08c98e2464afa7f4e0d118fd083c6a50
Umkaia <thomas@trheus.nl>
2018-01-15 11:13:40 +0000

    first test run

--- chubConnection.py
+++ chubConnection.py
@@ -1,90 +1,101 @@
 import smbus
 import time
+from random import randint
 
-bus = smbus.SMBus(1)
-loginAddr = 127
-addressArr = []
-currentHash = [255,255,255,255,255,255,255,255]
+class chubConnection:
+	bus = smbus.SMBus(1)
+	loginAddr = 127
+	addressArr = []
+	currentHash = [255,255,255,255,255,255,255,255]
+	
+	def __init__(self):
+		pass
 
-def loginRequest(loginAddr):
-    try:
-        bus.write_byte_data(loginAddr, 128, len(addressArr) + 1)
-        #time.sleep(1)
-        sig = bus.read_byte(loginAddr)
-        addr = loginResponse(currentHash) 
-        addressArr.append((addr,sig))
-        print(addressArr)
-    except:
-        print("no new module")
+	def loginRequest():
+		try:
+			bus.write_byte_data(loginAddr, 128, len(addressArr) + 1)
+			#time.sleep(1)
+			sig = bus.read_byte(loginAddr)
+			addr = loginResponse(currentHash) 
+			addressArr.append((addr,sig))
+			print(addressArr)
+			return sig
+		except:
+			print("no new module")
+		return -1;
 
-def loginResponse(sendHash):
-    bus.write_i2c_block_data(loginAddr, 127, sendHash)
-    temp_addr = len(addressArr) + 1
-    if bus.read_byte(temp_addr) == 200:
-        print("login successfull")
-        return temp_addr
+	def loginResponse(sendHash):
+		bus.write_i2c_block_data(loginAddr, 127, sendHash)
+		temp_addr = len(addressArr) + 1
+		if bus.read_byte(temp_addr) == 200:
+			print("login successfull")
+			return temp_addr
 
 
 
-def checkAlarmStatus():
-    for addr,sig in addressArr:
-        try:
-            bus.write_byte(addr, 125)
-            if bus.read_byte(addr) == 1:
-                print("alarm")
-            else:
-                print("no alarm")
+	def checkAlarmStatus():
+		for addr,sig in addressArr:
+			try:
+				bus.write_byte(addr, 125)
+				if bus.read_byte(addr) == 1:
+					print("alarm")
+					alarmed()
+					return sig				
+			except:
+				print("failure in statuscheck on: " + str(addr))
+		return 0
 
-        except:
-            print("failure in statuscheck on: " + addr)
+	def isAliveRequestAll():
+		for addr,sig in addressArr:
+			isAliveRequest(addr)
+			currentHash = generateNewHash()
+			isAliveResponse(addr, currentHash)
+			
+			
 
-def isAliveRequest():
-    for addr,sig in addressArr:
-        receivedHash = []
-        try:
-            bus.write_byte(addr, 124)
-            for x in range (0, 8):
-                receivedHash.append(bus.read_byte(addr))
-            print(receivedHash)
-        except Exception as e:
-            print("exception in requesting isAlive")
+	def generateNewHash():
+		tempHash = []
+		for x in range(0,8):
+			tempHash.append(randint(0,255))
+		return tempHash
 
-def isAliveResponse(address, sendHash):
-    try:
-        bus.write_i2c_block_data(address, 123, sendHash)
-    except Exception as e:
-        print("failure in isAliveResponse")
-        print e
+	def isAliveRequest(addr):
+		receivedHash = []
+		try:
+			bus.write_byte(addr, 124)
+			for x in range (0, 8):
+				receivedHash.append(bus.read_byte(addr))
+			print(receivedHash)
+		except Exception as e:
+			print("exception in requesting isAlive")
+		return receivedHash
 
-def alarm(address):
-    try:
-        bus.write_byte(address, 122)
-    except Exception as e:
-        print("cant ring alarm")
 
-def noAlarm(address):
-    try:
-	    bus.write_byte(address, 121)
-    except:
-	    print("alarm cant be reached")
+	def isAliveResponse(address, sendHash):
+		try:
+			bus.write_i2c_block_data(address, 123, sendHash)
+			print("sent this hash")
+			print(sendHash)
+		except Exception as e:
+			print("failure in isAliveResponse")
+			print e
 
-loginRequest(loginAddr)
-time.sleep(1)
-checkAlarmStatus()
-time.sleep(1)
-isAliveRequest()
-time.sleep(1)
-isAliveResponse(1, [1, 2, 10, 15, 166, 142, 200, 200])
-time.sleep(1)
-isAliveRequest()
-time.sleep(1)
-alarm(1)
-time.sleep(1)
-checkAlarmStatus()
-print("next device small test")
-time.sleep(30)
-loginRequest(loginAddr)
-time.sleep(1)
-loginResponse([1, 2, 3, 4, 5, 6, 7, 8])
-time.sleep(1)
-checkAlarmStatus()
+	def alarmed():
+		for addr,sig in addressArr:
+			alarm(addr)
+			
+	def alarm(address):
+		try:
+			bus.write_byte(address, 122)
+		except Exception as e:
+			print("cant ring alarm")
+
+	def dealarm():
+		for addr,sig in addressArr:
+			noAlarm(addr)
+
+	def noAlarm(address):
+		try:
+			bus.write_byte(address, 121)
+		except:
+			print("alarm cant be reached")
--- main.py
+++ main.py
@@ -1,52 +1,61 @@
 import storageHandler as sh
+import chubConnection as cc
 import time
 
 
 
 def create_module_from(_signature):
     modules = []
-    modules.append({'name':'Camera','type':'Observer'})
     modules.append({'name':'Siren','type':'Observer'})
     modules.append({'name':'Door','type':'Triggerer'})
     modules.append({'name':'Pressure','type':'Triggerer'})
     modules.append({'name':'Tripwire','type':'Triggerer'})
     modules.append({'name':'Motion','type':'Triggerer'})
-
-    mod_id = _signature % 10000
-    mod_info = _signature // 10000
-    
-    sh.add_module(modules[mod_info]['name']+str(mod_id),1,modules[mod_info]['type'],mod_id)
+	
+	if _signature == 9:
+		mod = modules[0]		 
+    else:
+		print(_signature)
+		
+    sh.add_module(mod['name'],1,mod['type'],_signature)
 
 
 def main_loop():
     time_start = time.time()
-
+	conn = cc.chubConnection()
     alive_counter = 300
     login_counter = 30
     test_counter = 0
     is_running = True
+    
+    alarm_latch = False
     while is_running:
         time_elapsed = time.time() - time_start
         if time_elapsed >= alive_counter:
             print("CHECK alive")
-            print(time_elapsed)
+            conn.isAliveRequestAll()
             alive_counter += 300
         elif time_elapsed >= login_counter:
-            print("login attempt")
-            print(time_elapsed)
+            sig = conn.loginRequest()
+            if sig != -1:
+				create_module_from(sig)
             login_counter += 30
         else:
             print("check whether the alarm is armed")
             if(sh.check_armed_status()):
-                print("Check alarm status")
+				trig = conn.checkAlarmStatus()
+				if trig != 0:
+					sh.add_event(trig)
+					#record and add location
+					conn.alarmed()
+					alarm_latch = True
+			else:
+				if alarm_latch:
+					conn.dealarm()
+					alarm_latch = False
             
         test_counter += 1
         if test_counter >= 660:
             is_running = False
         time.sleep(1)
 
-create_module_from(10000)
-
-create_module_from(20000)
-
-create_module_from(30000)
