220af324e1d0571942db92731f078c32bdc9ecb4
Umkaia <thomas@trheus.nl>
2018-01-17 14:01:27 +0000

    Fixed Camera Script for the Pi

--- camera_recording.py
+++ camera_recording.py
@@ -5,7 +5,7 @@ import time
 #Below code will capture X snapshots, store them in a specific folder with timestamps and create a short .MP4 movie with it
 
 #Directory that will receive all the shots + the recording video
-outputDir = '/Users/thibaud/Desktop'+time.strftime("/%Y-%m-%d_%Hh%Mm%Ss")
+outputDir = '/var/www/ChubFront/Files'+time.strftime("/%Y-%m-%d_%Hh%Mm%Ss")
 print("Creating folder..")
 os.makedirs(outputDir)
 number = 80
@@ -15,11 +15,11 @@ def record(number):
     count = number
     vidName = "recording" + '.mp4'
     # MP4 codec
-    fourcc = cv2.VideoWriter_fourcc(*'a\0\0\0')
+    #fourcc = cv2.VideoWriter_fourcc(*'MP4V')
     # Change current directory to output directory to save the video in the same folder as the snapshots.
     os.chdir(outputDir)
     # Create the video file
-    out = cv2.VideoWriter(vidName, fourcc, 20.0, (320, 176))
+    #out = cv2.VideoWriter(vidName, fourcc, 10.0, (320, 176))
 
     while(count > 0):
         # Access camera feed
@@ -34,24 +34,28 @@ def record(number):
             # Save each frame
             cv2.imwrite(os.path.join(outputDir,name), frame)
             # Add each frame to the video
-            out.write(frame)
+            #out.write(frame)
             count -= 1
             print("Saving snapshot "+str(80-count)+"/"+str(number)+"..")
     # Release video
-    out.release()
+    #out.release()
     # Release camera feed
+    print("Writing video..")
+    os.system('sudo avconv -r 10 -i snapshot%d.jpg -b:v 1000k recording.mp4')
+    print("Writing successful..")
     cap.release()
     cv2.destroyAllWindows()
+    
     print("End recording..")
 
-def test():
+def test(): 
     print("Start intruder recognition..")
     intruder = 0
-    face_cascade = cv2.CascadeClassifier('/Users/thibaud/Documents/Rotterdam/Project4/CHUB/CHUB/haarcascade_frontalface_default.xml')
-    eye_cascade = cv2.CascadeClassifier('/Users/thibaud/Documents/Rotterdam/Project4/CHUB/CHUB/haarcascade_eye.xml')
+    face_cascade = cv2.CascadeClassifier('/home/pi/Chub/CHUB/haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier('/home/pi/Chub/CHUB/haarcascade_eye.xml')
     for i in range(1,number):
         img = cv2.imread('snapshot'+str(i)+".jpg");
-        cv2.imshow('img',img)
+        #cv2.imshow('img',img)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
         for (x,y,w,h) in faces:
